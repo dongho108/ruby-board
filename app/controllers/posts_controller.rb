3ea658c04d7be4661f3904d93f1eb357f719aaef
김동호 <dongho.dh.kim@kurlycorp.com>
2025-02-02 17:57:42 +0900

    feat: 게시판 CRUD 추가

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: '게시글이 성공적으로 작성되었습니다.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: '게시글이 성공적으로 수정되었습니다.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_url, notice: '게시글이 삭제되었습니다.'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :author)
+  end
+end
